ccdc6a6d192fa2e2f63953b95174f2c91fe90751
Zain Ahmed <48980361+zainahmed0597@users.noreply.github.com>
2020-02-09 14:56:40 +0500

    First Commit

---
+++ Function Parameters 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int timesTwo( int x){
+	return x * 2;
+}
+
+
+int main(){
+	
+	cout << timesTwo(4) << endl;
+	
+	cout << timesTwo(8) << endl;
+	
+	cout << timesTwo(12)<< endl;	
+}
